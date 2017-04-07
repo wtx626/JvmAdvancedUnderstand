f82be6848ef9bea03cebe6b4f2698f7aa88adae0
yichen <597429882@qq.com>
2017-04-07 11:32:45 +0800

    yichen commit

---
+++ src/GCTest.java
@@ -0,0 +1,35 @@
+
+
+public class GCTest {
+	public void localvarGc1(){
+		byte[] a=new byte[6*1024*1024];
+		System.gc();
+	}
+	public void localvarGc2(){
+		byte[] a=new byte[6*1024*1024];
+		a=null;
+		System.gc();
+	}
+	public void localvarGc3(){
+		{
+		byte[] a=new byte[6*1024*1024];
+		}
+		System.gc();
+	}
+	public void localvarGc4(){
+		{
+			byte[] a=new byte[6*1024*1024];
+		}
+		int c=10;
+		System.gc();
+	}
+	public void localvarGc5(){
+		localvarGc1();
+		System.gc();
+	}
+	public static void main(String[] args) throws InterruptedException {
+		GCTest gct=new GCTest();
+		gct.localvarGc3();
+	}
+
+}
